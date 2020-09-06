2f638840505c0ab28c8f0cb0733c7dd3caa4e52c
Samir Sigh <samirsinghpat82@gmail.com>
2020-09-06 13:26:18 +0530

    Updates

---
+++ precache-manifest.6fc3d15d380c3d017f14ebc41b891d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a828cc8b628744498d2de8b0f32c23d",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "0eabab3a2776e3c2bf05",
+    "url": "/chat-app/static/css/main.dbd52189.chunk.css"
+  },
+  {
+    "revision": "01124f1aab16993ed383",
+    "url": "/chat-app/static/js/2.17ea84be.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chat-app/static/js/2.17ea84be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eabab3a2776e3c2bf05",
+    "url": "/chat-app/static/js/main.e335e5d8.chunk.js"
+  },
+  {
+    "revision": "844d941a2013a08fe2e0",
+    "url": "/chat-app/static/js/runtime-main.bdb3b2b2.js"
+  }
+]);
